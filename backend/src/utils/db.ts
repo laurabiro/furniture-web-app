60a9d83d73c58d3ddfed3d4a055bc9fc713613fb
laurabiro <laurabiro90@gmail.com>
2024-01-20 16:34:14 +0100

    resolve path

--- backend/src/utils/db.ts
+++ backend/src/utils/db.ts
@@ -1,11 +1,28 @@
 import fs from "fs/promises"
+import path from "path";
 
-const dbPath = `${__dirname}/../../database/`
+/* const dbPath = `${__dirname}/../../database/`
 
 export const load = async (filename: string): Promise<unknown> => {
   const data = await fs.readFile(`${dbPath}/${filename}.json`, "utf-8")
   return JSON.parse(data) as unknown
-}
+} */
+
+const dbPath = path.resolve(__dirname, '../../database/');
+
+export const load = async (filename: string): Promise<unknown> => {
+  const filePath = path.join(dbPath, `${filename}.json`);
+  
+  console.log('Resolved File Path:', filePath);
+
+  try {
+    const data = await fs.readFile(filePath, 'utf-8');
+    return JSON.parse(data) as unknown;
+  } catch (error) {
+    console.error('Error reading file:', error);
+    throw error;
+  }
+};
 
 export const save = (filename: string, data: JSONStringifyable): Promise<void> =>
   fs.writeFile(`${dbPath}/${filename}.json`, JSON.stringify(data, null, 2) , "utf-8")
